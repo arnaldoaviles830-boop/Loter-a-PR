7924a63e3410782b6986b9ef51f58bd6a72b46a9
arnaldoaviles830-boop <arnaldo.aviles830@gmail.com>
2026-09-11 21:31:43 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,12 +7,14 @@ self.addEventListener('install',event=>{
 });
 
 self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(
-    keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key))
-  )));
-  self.clients.claim();
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(
+        keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key))
+      ))
+      .then(()=>self.clients.claim())
+  );
 });
-
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
   const url=new URL(event.request.url);
